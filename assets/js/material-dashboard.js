1f28b80e08f47b3d5d77572dd9113836f66efc96
Riya Tyagi <59206566+Centrattic@users.noreply.github.com>
2021-01-30 00:01:13 -0500

    Update material-dashboard.js

--- assets/js/material-dashboard.js
+++ assets/js/material-dashboard.js
@@ -315,7 +315,7 @@ md = {
       dataDailySalesChart = {
         labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
         series: [
-          [12, 17, 7, 17, 23, 18, 38]
+          [164, 167, 170, 169, 173, 171, 170]
         ]
       };
 
@@ -323,8 +323,8 @@ md = {
         lineSmooth: Chartist.Interpolation.cardinal({
           tension: 0
         }),
-        low: 0,
-        high: 50, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
+        low: 100,
+        high: 200, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
         chartPadding: {
           top: 0,
           right: 0,
